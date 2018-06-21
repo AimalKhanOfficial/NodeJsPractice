181cd652ba606be0d3d30e29bb8893bb7ab07455
Aimal <aimalkhanofficial@gmail.com>
2018-06-21 16:10:36 -0500

    clearing interval in STYLE!

--- clearTimeoutAndClearInterval.js
+++ clearTimeoutAndClearInterval.js
@@ -1,3 +1,10 @@
-setInterval(function(){
-    console.log("hey bro");
+var counter = 0; //doesn't work if declaration removed, unlike JS
+var keyToInterval = setInterval(function(){
+    counter++;
+    console.log(counter+": hey bro");
+    if(counter == 10){
+        clearInterval(keyToInterval);
+    }
 }, 1000);
+
+console.log("Checking Async attribute!");
